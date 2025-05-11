71617e8b883d92cd815c0400bc61b539eaa37548
erinbest017 <126285687+erinbest017@users.noreply.github.com>
2025-05-11 13:03:16 -0700

    Add main, ACSDataImport, zipcodeToCensusTract files

--- Code/ACSDataImport.py
+++ Code/ACSDataImport.py
@@ -1,1 +1,63 @@
+""" 
+This File imports a variety of American Community Survey (ACS) 
+data tables from the United States Census Bureau.
+"""
 
+### Import Libraries 
+import requests
+import pandas as pd
+
+### Using APIs, import ACS data
+## Fetch Data Function
+def _fetch_data(url, params):
+    response = requests.get(url, params)
+    if response.status_code == 200:
+        data = response.json()
+        return pd.DataFrame(data[1:], columns = data[0])
+    else:
+        raise Exception(f'Failed to retrieve data: {response.status_code}')
+    
+## Median Income
+def getS1903():
+    url = 'https://api.census.gov/data/2023/acs/acs5/subject'
+    params = {
+        'get' : 'group(S1903)',
+        'ucgid' : 'pseudo(0400000US08$1400000)'
+    }
+    return _fetch_data(url, params)
+
+## Educational Attainment
+def getS1501():
+    url = 'https://api.census.gov/data/2023/acs/acs5/subject'
+    params = {
+        'get' : "group(S1501)",
+        'ucgid' : 'pseudo(0400000US08$1400000)'
+    }
+    return _fetch_data(url, params)
+
+## Households
+def getDP02():
+    url = 'https://api.census.gov/data/2023/acs/acs5/profile'
+    params = {
+        'get' : 'group(DP02)',
+        'ucgid' : 'pseudo(0400000US08$1400000)'
+    }
+    return _fetch_data(url, params)
+
+## Poverty Level
+def getB17001():
+    url = 'https://api.census.gov/data/2023/acs/acs5'
+    params = {
+        'get' : 'group(B17001)',
+        'ucgid' : 'pseudo(0400000US08$1400000)'
+    }
+    return _fetch_data(url, params)
+
+## Age
+def getS0101():
+    url = 'https://api.census.gov/data/2023/acs/acs5/subject'
+    params = {
+        'get' : 'group(S0101)',
+        'ucgid' : 'pseudo(0400000US08$1400000)'
+    }
+    return _fetch_data(url, params)
---
+++ Code/zipcodeToCensusTract.py
@@ -0,0 +1,60 @@
+""""
+This file takes a dataframe of NPI, zipcodes+4, and other information and adds the censustract
+to the df by converting the zipcode +4 to the appropriate censustract
+"""
+
+### Install if needed
+# !pip install pgeocode
+# !pip install censusgeocode
+
+### Import Libraries 
+import pandas as pd
+import numpy as np
+import pgeocode as pg
+import censusgeocode as cg
+import time
+
+################# TO BE REPLACED WITH DAGMAR'S WORK #################
+## TEST DATA
+testData = {'NPI': ['1760093470', '1861097982', '1447936901', '1043826795'], 
+            'zipPlusFour': ['80237-2857', '81008-2130', '80111-2213', '80111-7957'],    ##Last zipcode is not accurate - just used for testing
+            'taxType': ['NPI-2 Organization', 'NPI-2 Organization', 'NPI-2 Organization', 'NPI-2 Organization'], 
+            'primaryTax': ['Yes', 'Yes', 'Yes', 'No'], 
+            'Issuer': ['MEDICAID', '', '', ''], 
+            'status': ['Active', 'Active', 'Active', 'Active']}
+
+## CREATE TEST DF
+df = pd.DataFrame(testData)
+
+####################################################################
+
+
+## Function - postal code to lat/long
+def convertPostalToLatLong(zipcode):
+    postal = pg.Nominatim('us')
+    lat = postal.query_postal_code(zipcode)['latitude']
+    long = postal.query_postal_code(zipcode)['longitude']
+    return lat, long
+
+## Function - lat/long to Census Tract
+def convertLatLongToCensusTract(lat, long):
+    time.sleep(0.1)
+    try:
+        result = cg.coordinates(x = long, y = lat)
+        censusTract = result['Census Tracts'][0]['GEOID']
+        censusTract = '1400000US' + censusTract
+        return censusTract
+    except Exception as e:
+        print(f'Error {e} at lat = {lat}, long = {long}')
+        return None
+    
+################# WE MIGHT BE ABLE TO DELETE THE BIT BELOW
+## Create zipcode column
+df['zip'] = df['zipPlusFour'].str[:5]
+
+## Call function and add lat/long to df
+df[['lat', 'long']] = df['zip'].apply(lambda zip: pd.Series(convertPostalToLatLong(zip)))
+
+## Call function and censustract to df
+df['censusTract'] = df.apply(lambda row: convertLatLongToCensusTract(row['lat'], row['long']), axis = 1)
+df
